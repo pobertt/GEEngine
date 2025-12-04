4df9b6c256752e79c0ac2a6629cd6c6cea0d1ce2
pobertt <79865756+pobertt@users.noreply.github.com>
2025-12-04 13:43:47 +0000

    trex is now on screen but no animations

--- GEEngineUpdated/GEEngineUpdated/VSAnim.hlsl
+++ GEEngineUpdated/GEEngineUpdated/VSAnim.hlsl
@@ -1,46 +1,53 @@
-cbuffer staticMeshBuffer
+// VSAnim.hlsl
+
+cbuffer staticMeshBuffer : register(b0)
 {
     float4x4 W;
     float4x4 VP;
-    float4x4 bones[256];
+    float4x4 bones[256]; // Array of bone matrices sent from C++
 };
 
 struct VS_INPUT
 {
-    float4 Pos : POSITION;
+    float3 Pos : POSITION;
     float3 Normal : NORMAL;
     float3 Tangent : TANGENT;
     float2 TexCoords : TEXCOORD;
-    uint4 BoneIDs : BONEIDS;
-    float4 BoneWeights : BONEWEIGHTS;
+    uint4 BoneIDs : BONEIDS; // Must match C++ Layout name
+    float4 Weights : BONEWEIGHTS; // Must match C++ Layout name
 };
 
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
     float3 Normal : NORMAL;
-    float3 Tangent : TANGENT;
     float2 TexCoords : TEXCOORD;
 };
 
 PS_INPUT VS(VS_INPUT input)
 {
     PS_INPUT output;
-    float4 pos = input.Pos;
-    float4x4 transform;
-    transform = bones[input.BoneIDs[0]] * input.BoneWeights[0];
-    transform += bones[input.BoneIDs[1]] * input.BoneWeights[1];
-    transform += bones[input.BoneIDs[2]] * input.BoneWeights[2];
-    transform += bones[input.BoneIDs[3]] * input.BoneWeights[3];
-    output.Pos = mul(pos, transform);
-    output.Pos = mul(output.Pos, W);
-    output.Pos = mul(output.Pos, VP);
-    output.Normal = mul(input.Normal, (float3x3) transform);
-    output.Normal = mul(output.Normal, (float3x3) W);
-    output.Normal = normalize(output.Normal);
-    output.Tangent = mul(input.Tangent, (float3x3) transform);
-    output.Tangent = mul(output.Tangent, (float3x3) W);
-    output.Tangent = normalize(output.Tangent);
+
+    // Calculate Skinned Position
+    // Sum of (BoneMatrix * Weight) for all 4 influencing bones
+    float4x4 boneTransform = bones[input.BoneIDs[0]] * input.Weights[0];
+    boneTransform += bones[input.BoneIDs[1]] * input.Weights[1];
+    boneTransform += bones[input.BoneIDs[2]] * input.Weights[2];
+    boneTransform += bones[input.BoneIDs[3]] * input.Weights[3];
+
+    // Apply Bone Transform to local position
+    float4 posLocal = mul(float4(input.Pos, 1.0f), boneTransform);
+    
+    // Apply World and View-Projection
+    float4 posWorld = mul(posLocal, W);
+    output.Pos = mul(posWorld, VP);
+
+    // Transform Normal (Rotate only)
+    // Note: Technically should use InverseTranspose of World for non-uniform scale
+    float3 normalLocal = mul(input.Normal, (float3x3) boneTransform);
+    output.Normal = mul(normalLocal, (float3x3) W);
+
     output.TexCoords = input.TexCoords;
+
     return output;
 }
